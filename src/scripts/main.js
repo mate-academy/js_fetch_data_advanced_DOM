2dfff0342c78157b9f9e58d5ee95614f3afa071b
Petro Mandziuk <64119700+Petro-JS-Developer@users.noreply.github.com>
2020-10-23 15:29:23 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,76 @@
 'use strict';
 
-// write code here
+const BASE_URL = 'https://mate-academy.github.io/phone-catalogue-static/api';
+const arrPromises = [];
+const arrPromisesWith3Element = [];
+
+const request = (url) => {
+  // eslint-disable-next-line no-undef
+  return fetch(`${BASE_URL}${url}`)
+    .then(response => {
+      if (!response.ok) {
+        // eslint-disable-next-line no-throw-literal
+        throw `${response.status} - ${response.statusText}`;
+      }
+
+      return response.json();
+    });
+};
+
+request('/phones.json')
+  .then(listPhones => {
+    const phonesIds = listPhones.map(phone => phone.id);
+
+    getFirstReceivedDetails(phonesIds)
+      .then((IdFirstPromise) => {
+        request(`/phones/${IdFirstPromise}.json`)
+          .then(details => {
+            console.log(details);
+          });
+      });
+
+    getAllSuccessfulDetails(phonesIds)
+      .then(arrDetails => console.log(arrDetails));
+
+console.log(getThreeFastestDetails(phonesIds))
+    
+    // getThreeFastestDetails(phonesIds)
+      // .then(results => console.log(results));
+  });
+
+function getFirstReceivedDetails(ids) {
+  ids.forEach(id => {
+    const newPromise = new Promise(resolve => {
+      setTimeout(() => {
+        resolve(id);
+      }, Math.round(Math.random() * 1000));
+    });
+
+    arrPromises.push(newPromise);
+  });
+
+  return Promise.race(arrPromises);
+}
+
+function getAllSuccessfulDetails(arrIds) {
+  return Promise.all(
+    arrIds.map(id => {
+      return request(`/phones/${id}.json`)
+        .then(response => response);
+    })
+  );
+}
+
+// function getThreeFastestDetails(arrayIds) {
+//   arrayIds.forEach(id => {
+//     const newPromise = new Promise(resolve => {
+//       setTimeout(() => {
+//         resolve(id);
+//       }, Math.round(Math.random() * 1000));
+//     });
+
+//     arrPromisesWith3Element.push(newPromise);
+//   });
+
+//   return Promise.all(arrPromisesWith3Element);
+// }
