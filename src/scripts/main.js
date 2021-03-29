01a4b1d85d2e7cb7c3846caeb9f16095b78ff4a3
Yevgeniy Podobreev <podobr_2018@ukr.net>
2021-03-30 00:45:52 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,107 @@
 'use strict';
 
 // write code here
+
+const url = `${
+  'https://mate-academy.github.io/phone-catalogue-static/api/phones.json'
+}`;
+
+const body = document.querySelector('body');
+
+const createList = (phones, classN, headText) => {
+  const divAll = document.createElement('div');
+
+  divAll.className = classN;
+
+  const heading = document.createElement('h3');
+
+  heading.textContent = headText;
+
+  const list = document.createElement('ul');
+
+  for (const phone of phones) {
+    const li = document.createElement('li');
+
+    li.textContent = `${phone.id.toUpperCase()} ${phone.name}`;
+    list.append(li);
+  }
+  divAll.append(heading);
+  divAll.append(list);
+  body.append(divAll);
+};
+
+const createElem = (phone, classN, headText) => {
+  const divFirst = document.createElement('div');
+
+  divFirst.className = classN;
+
+  const heading = document.createElement('h3');
+
+  heading.textContent = headText;
+
+  const li = document.createElement('li');
+
+  li.textContent = `${phone.id} ${phone.name}`;
+  divFirst.append(heading);
+  divFirst.append(li);
+  body.append(divFirst);
+};
+
+const getPhones = () => {
+  return new Promise((resolve, reject) => {
+    fetch(`${url}`)
+      .then(response => {
+        return response.json();
+      })
+      .then(response => {
+        return resolve(response);
+      });
+  });
+};
+
+const details = `${
+  'https://mate-academy.github.io/phone-catalogue-static/api/phones'
+}`;
+
+const getFirstReceivedDetails = (arrayIds) => {
+  return Promise.race(arrayIds.map((id) => {
+    return new Promise((resolve, reject) => {
+      fetch(`${details}/${id}.json`)
+        .then((response) => {
+          return resolve(response.json());
+        });
+    });
+  }));
+};
+
+const getAllSuccessfulDetails = (arrayIds) => {
+  return Promise.all(arrayIds.map((id) => {
+    return new Promise((resolve, reject) => {
+      fetch(`${details}/${id}.json`)
+        .then((response) => {
+          return resolve(response.json());
+        });
+    });
+  }));
+};
+
+getPhones()
+  .then((result) => {
+    const arrayPhonesId = result.map((phone) => {
+      return phone.id;
+    });
+
+    getAllSuccessfulDetails(arrayPhonesId)
+      .then((resolve) => {
+        createList(result, `all-successful`, `All Successful`);
+
+        return resolve;
+      });
+
+    getFirstReceivedDetails(arrayPhonesId)
+      .then((resolve) => {
+        createElem(resolve, `first-received`, `First Received`);
+
+        return resolve;
+      });
+  });
