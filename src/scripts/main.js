3a47e4dc49a2af30cc39aeff8b157a0e2fc98cbc
MaximTsyhanenko <maksym.tsyganenko85@gmail.com>
2021-10-19 22:03:24 +0300

    solution v.2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,45 +1,45 @@
 'use strict';
 
-// const arrIds = [
-//   'motorola-xoom-with-wi-fi',
-//   'dell-streak-7',
-//   'samsung-gem',
-// ];
-
 /* eslint-disable-next-line */
-const BASEurl = 'https://mate-academy.github.io/phone-catalogue-static/api/phones/';
+const BASEurl = 'https://mate-academy.github.io/phone-catalogue-static/api/phones';
 
 function request(phoneId) {
-  return fetch(BASEurl + phoneId + '.json')
+  return fetch(BASEurl + '/' + phoneId + '.json')
     .then(response => {
       if (!response.ok) {
-        return null;
+        return Promise.reject(new Error('promise rejected'));
       }
 
       return response.json();
     })
-    .catch(new Error('Function "request" done with error'));
+    .catch('Function "request" done with error');
 }
 
-/* eslint-disable-next-line */
+function createPhonesList(listClass, header) {
+  const div = document.createElement('div');
+
+  div.classList.add(listClass);
+
+  div.insertAdjacentHTML('beforeend', `
+    <h1>${header}</h1>
+  `);
+
+  document.body.append(div);
+}
+
 function getFirstReceivedDetails(IdList) {
-  const phones = IdList.map(phoneId => {
-    return request(phoneId);
-  });
+  const phones = IdList.map(phoneId => request(phoneId));
 
   return Promise.race(phones)
     .then(result => {
-      const div = document.createElement('div');
+      createPhonesList('first-received', 'First Received');
 
-      div.classList.add('first-received');
+      const div = document.getElementsByClassName('first-received')[0];
 
       div.insertAdjacentHTML('beforeend', `
-        <h1>First Received</h1>
-        <p>ID: ${result.id}</p>
-        <p>Name: ${result.name}</p>
+        <p><strong>ID:</strong> ${result.id.toUpperCase()}</p>
+        <p><strong>Name:</strong> ${result.name}</p>
       `);
-
-      document.body.append(div);
     })
     .catch(() => {
       document.body.insertAdjacentHTML('beforeend', `
@@ -48,31 +48,24 @@ function getFirstReceivedDetails(IdList) {
     });
 }
 
-/* eslint-disable-next-line */
 function getAllSuccessfulDetails(IdList) {
-  const phones = IdList.map(phoneId => {
-    return request(phoneId);
-  });
+  const phones = IdList.map(phoneId => request(phoneId));
 
-  return Promise.all(phones)
-    .then(phonesArray => phonesArray.filter(phone => phone !== null))
+  return Promise.allSettled(phones)
+    .then(phonesArray => phonesArray
+      .filter(phone => phone.status !== 'rejected'))
     .then(result => {
-      const div = document.createElement('div');
+      createPhonesList('all-successful', 'All Successful');
 
-      div.classList.add('all-successful');
-
-      div.insertAdjacentHTML('beforeend', `
-        <h1>All Successful</h1>
-      `);
+      const div = document.getElementsByClassName('all-successful')[0];
 
       for (const phone of result) {
         div.insertAdjacentHTML('beforeend', `
-          <p>ID: ${phone.id}</p>
-          <p>Name: ${phone.name}</p>
+          <p><strong>ID:</strong> ${phone.value.id.toUpperCase()}</p>
+          <p><strong>Name:</strong> ${phone.value.name}</p>
+          <p>____________</p>
         `);
       }
-
-      document.body.append(div);
     })
     .catch(() => {
       document.body.insertAdjacentHTML('beforeend', `
@@ -80,3 +73,59 @@ function getAllSuccessfulDetails(IdList) {
       `);
     });
 }
+
+function getThreeFastestDetails(IdList) {
+  const phones = IdList.map(phoneId => request(phoneId));
+
+  createPhonesList('fastest', 'Fastest Responses');
+
+  const fastestResponse = (data) => {
+    return Promise.race(data)
+      .then(result => {
+        const div = document.getElementsByClassName('fastest')[0];
+
+        div.style.color = 'green';
+        div.style.position = 'absolute';
+        div.style.bottom = '10px';
+        div.style.left = '55px';
+        div.style.fontSize = '13px';
+
+        div.insertAdjacentHTML('beforeend', `
+          <p><strong>ID:</strong> ${result.id.toUpperCase()}</p>
+          <p><strong>Name:</strong> ${result.name}</p>
+          <p>____________</p>
+        `);
+      })
+      .catch(() => {
+        document.body.insertAdjacentHTML('beforeend', `
+          <div>getFirstReceivedDetails rejected!</div>
+        `);
+      });
+  };
+
+  for (let i = 0; i < 3; i++) {
+    fastestResponse(phones);
+  }
+}
+
+function getInfoAboutPhones(url) {
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        return Promise.reject(new Error('promise rejected'));
+      }
+
+      return response.json();
+    })
+    .then(result => {
+      return result.map(phone => phone.id);
+    })
+    .then(phonesId => {
+      getFirstReceivedDetails(phonesId);
+      getAllSuccessfulDetails(phonesId);
+      getThreeFastestDetails(phonesId);
+    })
+    .catch('Function "getPhonesId" done with error');
+}
+
+getInfoAboutPhones(BASEurl + '.json');
