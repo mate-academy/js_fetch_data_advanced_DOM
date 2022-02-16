49a28b15239ae70b0e6bc64d83fa8b94376f7a01
Ladyha Roman <ladrom.ua@gmail.com>
2022-02-16 21:49:10 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,76 @@
 'use strict';
 
-// write code here
+const link
+  = 'https://mate-academy.github.io/phone-catalogue-static/api/phones.json';
+
+function getPhones() {
+  return new Promise((resolve, reject) => {
+    fetch(link)
+      .then(result => {
+        return resolve(result.json());
+      })
+      .catch(error => reject(new Error('Error', error)));
+  });
+}
+
+function getFirstReceivedDetails() {
+  return new Promise((resolve, reject) => {
+    getPhones()
+      .then(result => {
+        return Promise.race(result);
+      })
+      .then(result => {
+        return resolve([result]);
+      })
+      .catch(error => reject(new Error('Error', error)));
+  });
+}
+
+function getAllSuccessfulDetails() {
+  return new Promise((resolve, reject) => {
+    getPhones()
+      .then(result => {
+        return resolve(Promise.all(result));
+      })
+      .catch(error => reject(new Error('Error', error)));
+  });
+}
+
+function getThreeFastestDetails() {
+  return new Promise((resolve, reject) => {
+    getPhones()
+      .then(result => {
+        return resolve(Promise.all([
+          Promise.race(result),
+          Promise.race(result),
+          Promise.race(result),
+        ]));
+      })
+      .catch(error => reject(new Error('Error', error)));
+  });
+}
+
+function createElement(divClass, title, func) {
+  const div = document.createElement('div');
+
+  document.body.append(div);
+  div.classList.add(divClass);
+
+  div.insertAdjacentHTML('beforeend', `
+    <h2>${title}</h2>
+    <ul class="list"></ul>
+  `);
+
+  func()
+    .then(result => {
+      result.map(item => {
+        div.children[1].insertAdjacentHTML('beforeend', `
+          <li>${item.name} - ${item.id}</li>
+        `);
+      });
+    });
+}
+
+createElement('all-successful', 'All Successful', getAllSuccessfulDetails);
+createElement('first-received', 'First Received', getFirstReceivedDetails);
+createElement('three-fatest', 'Three Fastest Received', getThreeFastestDetails);
