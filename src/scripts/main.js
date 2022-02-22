fccc6a78a3ee0261dce7596daaf9372394eee010
Roman Kovalchuk <roma220702@gmail.com>
2022-02-22 19:40:39 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,116 @@
 'use strict';
 
-// write code here
+const link = `https://mate-academy.github.io/
+phone-catalogue-static/api/phones.json`;
+const linkBase = `https://mate-academy.github.io/
+phone-catalogue-static/api/phones/`;
+
+function getPhones(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(result => {
+        return resolve(result.json());
+      })
+      .catch(error => reject(new Error('Error', error)));
+  });
+}
+
+function getIds() {
+  return getPhones(link)
+    .then(result => {
+      return result.map(phone => {
+        return phone.id;
+      });
+    });
+}
+
+function getAllSuccessfulDetails(massId, resolves) {
+  return new Promise(function(resolve, reject) {
+    getIds()
+      .then(result => {
+        return result.map(item => {
+          return getPhones(`${linkBase}${item}.json`);
+        });
+      })
+      .then(result => {
+        return Promise.allSettled(result);
+      })
+      .then(result => {
+        const mass = [];
+
+        result.forEach(element => {
+          if (element.status === 'fulfilled') {
+            mass.push(element.value);
+          }
+        });
+
+        return resolve(mass);
+      })
+      .catch(error => reject(new Error('Error', error)));
+  });
+}
+
+function getFirstReceivedDetails(massId, resolves) {
+  return new Promise(function(resolve, reject) {
+    getIds()
+      .then(result => {
+        return result.map(item => {
+          return getPhones(`${linkBase}${item}.json`);
+        });
+      })
+      .then(result => {
+        return Promise.rase(result);
+      })
+      .then(result => {
+        return resolve([result]);
+      })
+      .catch(error => reject(new Error('Error', error)));
+  });
+}
+
+function getThreeFastestDetails(massId, resolves) {
+  return new Promise(function(resolve, reject) {
+    getIds()
+      .then(result => {
+        return result.map(item => {
+          return getPhones(`${linkBase}${item}.json`);
+        });
+      })
+      .then(result => {
+        return Promise.all([
+          Promise.race(result),
+          Promise.race(result),
+          Promise.race(result),
+        ]);
+      })
+      .then(result => {
+        return resolve(result);
+      })
+      .catch(error => reject(new Error('Error', error)));
+  });
+}
+
+function createElement(divClass, title, func) {
+  const div = document.createElement('div');
+
+  document.body.append(div);
+  div.classList.add(divClass);
+
+  div.insertAdjacentHTML('beforeend', `
+    <h2>${title}</h2>
+    <ul class="list"></ul>
+  `);
+
+  func()
+    .then(result => {
+      result.map(item => {
+        div.children[1].insertAdjacentHTML('beforeend', `
+          <li>${item.name} - ${item.id}</li>
+        `);
+      });
+    });
+}
+
+createElement('all-successful', 'All Successful', getAllSuccessfulDetails);
+createElement('first-received', 'First Received', getFirstReceivedDetails);
+createElement('three-fatest', 'Three Fastest Received', getThreeFastestDetails);
