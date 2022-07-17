3eac662236bbfbc831f19458c2e4e51eb219f919
Dmytro Lebedchenko <lebedchenkodmytro@gmail.com>
2022-07-17 17:27:58 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,93 @@
 'use strict';
 
-// write code here
+const BASE_URL = 'https://mate-academy.github.io/phone-catalogue-static/api';
+
+function createDiv(divClass, divTitle, divListItem) {
+  document.body.insertAdjacentHTML('afterbegin', `
+    <div class="${divClass}">
+      <h2 style="text-decoration: underline; color: #fc4b3b">${divTitle}</h2>
+      ${divListItem}
+    </div>
+  `);
+}
+
+const request = (url, timeout = 0) => {
+  return fetch(`${BASE_URL}${url}.json`)
+    .then(response => {
+      setTimeout(() => {
+        if (!response.ok) {
+          return `${response.status} - ${response.statusText}`;
+        }
+      }, timeout);
+
+      return response.json();
+    });
+};
+
+const getFirstReceivedDetails = () => {
+  request('/phones')
+    .then(phones => {
+      return Promise.race(phones.map(phone => request(`/phones/${phone.id}`)));
+    })
+    .then(phones => {
+      createDiv(
+        'first-received',
+        'First Received',
+        [phones].map(element => `
+          <span>
+            [Name]: ${element.name}
+            <br>
+            [ID]: ${element.id}
+          </span>
+        `));
+    });
+};
+
+const getThreeFastestDetails = () => {
+  request('/phones')
+    .then(phones => {
+      return Promise.allSettled([
+        Promise.race(phones.map(phone => request(`/phones/${phone.id}`))),
+        Promise.race(phones.map(phone => request(`/phones/${phone.id}`))),
+        Promise.race(phones.map(phone => request(`/phones/${phone.id}`))),
+      ]);
+    })
+    .then(phones => {
+      createDiv(
+        'three-fastest-received',
+        'Three Fastest',
+        phones.map(element => `
+          <li>
+            [Name]: ${element.value.name}
+            <br>
+            [ID]: ${element.value.id}
+          </li>
+          <br>
+        `).join(''));
+    });
+};
+
+const getAllSuccessfulDetails = () => {
+  request('/phones')
+    .then(phones => {
+      return Promise.allSettled(
+        phones.map(phone => request(`/phones/${phone.id}`)));
+    })
+    .then(phones => {
+      createDiv(
+        'all-successful',
+        'All Successful',
+        [...phones].map(element => `
+          <li>
+            [Name]: ${element.value.name}
+            <br>
+            [ID]: ${element.value.id}
+          </li>
+          <br>
+      `).join(''));
+    });
+};
+
+getFirstReceivedDetails();
+getThreeFastestDetails();
+getAllSuccessfulDetails();
