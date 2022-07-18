6725eacd23ff81abad501f8f80ed8d8af955110a
Max Shylenko <maxshylenko@gmail.com>
2022-07-18 21:44:22 +0200

    fetch data advanced DOM done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,100 @@
 'use strict';
 
-// write code here
+const BASE_URL = 'https://mate-academy.github.io/'
+  + 'phone-catalogue-static/api/phones.json';
+const DETAILS = 'https://mate-academy.github.io/'
+  + 'phone-catalogue-static/api/phones/:phoneId.json';
+
+const body = document.querySelector('body');
+
+const createList = (className, headerText) => {
+  const detail = document.createElement('ul');
+  const header = document.createElement('h1');
+
+  detail.className = className;
+  header.innerText = headerText;
+
+  detail.append(header);
+  body.insertBefore(detail, body.lastElementChild);
+};
+
+const createListItem = (id, nameOfPhone, className) => {
+  const phone = document.createElement('li');
+
+  phone.innerText = `ID: ${id} NAME: ${nameOfPhone}`;
+
+  body.querySelector(`.${className}`).append(phone);
+};
+
+function requestIds() {
+  return fetch(BASE_URL)
+    .then(response => response.json())
+    .then(phones => {
+      const phonesIds = phones.map(phone => phone.id);
+
+      getFirstReceivedDetails(phonesIds)
+        .then(fastestDetails => {
+          createList('first-received', 'First Received');
+
+          createListItem(fastestDetails.id, fastestDetails.name,
+            'first-received');
+        });
+
+      getAllSuccessfulDetails(phonesIds)
+        .then(details => {
+          createList('all-successful', 'All Successful');
+
+          details.forEach(detail => {
+            createListItem(detail.id, detail.name, 'all-successful');
+          });
+        });
+
+      getThreeFastestDetails(phonesIds)
+        .then(details => {
+          createList('three-fastest-successful', 'Three Fastest Successful');
+
+          const listOfThreeFastest
+            = body.querySelector('.three-fastest-successful');
+
+          listOfThreeFastest.style.position = 'absolute';
+          listOfThreeFastest.style.top = '60%';
+          listOfThreeFastest.style.left = '50px';
+          listOfThreeFastest.style.backgroundColor = 'orange';
+          listOfThreeFastest.style.padding = '15px';
+          listOfThreeFastest.style.borderRadius = '15px';
+
+          details.forEach(detail => {
+            createListItem(detail.id, detail.name, 'three-fastest-successful');
+          });
+        });
+    });
+};
+
+function getFirstReceivedDetails(ids) {
+  return Promise.race(ids.map(id => {
+    return fetch(DETAILS.replace(':phoneId', id))
+      .then(details => details.json());
+  }));
+};
+
+function getAllSuccessfulDetails(ids) {
+  return Promise.all(ids.map(id => {
+    return fetch(DETAILS.replace(':phoneId', id))
+      .then(details => details.json());
+  }));
+};
+
+function getThreeFastestDetails(ids) {
+  const listOfdetails = ids.map(id => {
+    return fetch(DETAILS.replace(':phoneId', id))
+      .then(details => details.json());
+  });
+
+  return Promise.all([
+    Promise.race(listOfdetails),
+    Promise.race(listOfdetails),
+    Promise.race(listOfdetails),
+  ]);
+};
+
+requestIds();
