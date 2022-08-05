fe09b52a127dac5f8f55efc22b7175ba272a37b6
Tania Troshchuk <troschuk95@gmail.com>
2022-08-05 18:50:22 +0300

    Solution Task: Fetch data (advanced)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,76 @@
 'use strict';
 
-// write code here
+// eslint-disable-next-line max-len
+const baseURL = 'https://mate-academy.github.io/phone-catalogue-static/api/phones';
+
+function request(url) {
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
+
+      return response.json();
+    });
+}
+
+function createList(arr, className, title) {
+  document.body.insertAdjacentHTML('beforeend', `
+    <ul class="${className}">
+      <h1>${title}</h1>
+
+      ${arr.map((phone) => `
+        <li>${phone.id.toUpperCase()}</li>
+      `).join('')}
+    </ul>
+  `);
+}
+
+function getAllSuccessfulDetails(arr) {
+  const allPromises = arr.map((id) => {
+    return request(`${baseURL}/${id}.json`);
+  });
+
+  Promise.all(allPromises).then(phones => {
+    createList(phones, 'all-successful', 'All Successful text');
+  });
+}
+
+function getFirstReceivedDetails(arr) {
+  const firstPromise = arr.map((id) => {
+    return request(`${baseURL}/${id}.json`);
+  });
+
+  Promise.race(firstPromise).then(phone => {
+    createList([phone], 'first-received ', 'First Received');
+  });
+}
+
+function getThreeFastestDetails(arr) {
+  const allPromises = arr.map((id) => {
+    return request(`${baseURL}/${id}.json`);
+  });
+
+  const threeFirstPronise = allPromises.slice(0, 3);
+
+  Promise.all(threeFirstPronise).then(phone => {
+    createList(phone, 'first-tree-received ', 'First Tree Received');
+  });
+}
+
+const getPhones = () => {
+  return request(`${baseURL}.json`)
+    .then(phones => {
+      return phones;
+    });
+};
+
+getPhones()
+  .then(phones => {
+    const ids = phones.map(({ id }) => id);
+
+    getAllSuccessfulDetails(ids);
+    getFirstReceivedDetails(ids);
+    getThreeFastestDetails(ids);
+  })
+  .catch(error => new Error('Error:', error));
