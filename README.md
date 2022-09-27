3fea19a8fc7a77924ac3b687c9e4db35a4634675
Tilak Kumarclear <mailtotilakkumar@gmail.com>
2022-09-27 23:55:28 +0530

    solution

--- README.md
+++ README.md
@@ -1,25 +1,28 @@
 1. Replace `<your_account>` with your Github username in the link
-    - [DEMO LINK](https://<your_account>.github.io/js_fetch_data_advanced_DOM/)
+   - [DEMO LINK](https://Quelaan1.github.io/js_fetch_data_advanced_DOM/)
 2. Follow [this instructions](https://mate-academy.github.io/layout_task-guideline/)
-    - Run `npm run test` command to test your code;
-    - Run `npm run test:only -- -n` to run fast test ignoring linter;
-    - Run `npm run test:only -- -l` to run fast test with additional info in console ignoring linter.
+   - Run `npm run test` command to test your code;
+   - Run `npm run test:only -- -n` to run fast test ignoring linter;
+   - Run `npm run test:only -- -l` to run fast test with additional info in console ignoring linter.
 
 ### Task: Fetch data (advanced)
 
 API Url:
+
 - Details URL: https://mate-academy.github.io/phone-catalogue-static/api/phones/:phoneId.json
 
 The main goal of this task is an advanced using of Promises.
 
 Create next functions:
+
 - `getFirstReceivedDetails` which takes array of phone's ID and `resolves` with the first received detail (the fastest response NOT the first in the list). Ignore the other responses;
 - `getAllSuccessfulDetails` which takes array of phones' IDs and `resolves` with an array of all successfully received details (errors should be ignored).
 
 (optional) `getThreeFastestDetails` which takes array of phones IDs and `resolves` with an array of the details for the first 3 responses (the fastest).
 
-Hints: 
-- You have to use DOM to notify users about resolved or rejected promises; 
+Hints:
+
+- You have to use DOM to notify users about resolved or rejected promises;
 - Create <div> elements with classes `first-received` and `all-successful` which contain list of phones;
 - Each <div> element should have <h3> headings with `First Received` or `All Successful` text;
-- All IDs and names of phones should be inside of  <li> elements.
+- All IDs and names of phones should be inside of <li> elements.
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,85 @@
 'use strict';
 
-// write code here
+const baseUrl = 'https://mate-academy.github.io/phone-catalogue-static/api';
+
+const request = (url) => {
+  return fetch(`${baseUrl}${url}`).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    return response.json();
+  });
+};
+
+const getPhones = () => {
+  return request(`/phones.json`);
+};
+
+const getDetails = (phoneId) => {
+  return request(`/phones/${phoneId}.json`);
+};
+
+const getPhonesDetails = (option) => {
+  return getPhones().then((phone) => {
+    const phonesIdList = [];
+
+    phone.forEach((data) => {
+      phonesIdList.push(getDetails(data.id));
+    });
+
+    if (option === 'first') {
+      return Promise.race(phonesIdList);
+    }
+
+    return Promise.allSettled(phonesIdList);
+  });
+};
+
+const addPhonesList = (title, className, phonesList) => {
+  const container = document.createElement('div');
+
+  container.className = className;
+  container.innerHTML = `<h2>${title}</h2>`;
+
+  const list = document.createElement('ul');
+
+  container.append(list);
+  document.body.append(container);
+
+  phonesList.forEach((phone) => {
+    list.insertAdjacentHTML(
+      'beforeend',
+      `
+      <li>${phone.name} <span hidden>${phone.id.toUpperCase()}</span></li>
+    `
+    );
+  });
+};
+
+const getFirstReceivedDetails = () => {
+  getPhonesDetails('first').then((data) => {
+    addPhonesList('First Received', 'first-received', [data]);
+  });
+};
+
+const getAllSuccessfulDetails = () => {
+  getPhonesDetails().then((data) => {
+    const dataMapped = data.map(({ value }) => value);
+
+    addPhonesList('All Successful', 'all-successful', dataMapped);
+  });
+};
+
+const getThreeFastestDetails = () => {
+  getPhonesDetails().then((data) => {
+    const dataSliced = data.slice(0, 3);
+    const dataMapped = dataSliced.map(({ value }) => value);
+
+    addPhonesList('First Three', 'first-three', dataMapped);
+  });
+};
+
+getFirstReceivedDetails();
+getAllSuccessfulDetails();
+getThreeFastestDetails();
